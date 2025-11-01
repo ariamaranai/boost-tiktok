127c95e33aabf4494138ffa23d91b9fe47e12034
a <106607480+ariamaranai@users.noreply.github.com>
2025-11-02 02:57:34 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,5 +1,1 @@
-chrome.omnibox.onInputEntered.addListener(q =>
-  chrome.tabs.update({
-    url: "https://www.tiktok.com/search?q=" + q
-  })
-);
+chrome.omnibox.onInputEntered.addListener(q => chrome.tabs.update({ url: "https://www.tiktok.com/search?q=" + q }));
